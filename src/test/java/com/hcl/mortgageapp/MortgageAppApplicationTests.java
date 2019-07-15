66700949cc8b3605366b1518fdc94ae36a5ea825
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-15 15:10:23 +0530

    sagar changes3

--- src/test/java/com/hcl/mortgageapp/MortgageAppApplicationTests.java
+++ src/test/java/com/hcl/mortgageapp/MortgageAppApplicationTests.java
@@ -19,6 +19,12 @@ public class MortgageAppApplicationTests {
 	public void contextLoads() 
 	{
 		System.out.println("Welcome");
+		
+		System.out.println("Welcome");
+		System.out.println("Welcome");
+		System.out.println("Welcome");
+		System.out.println("Welcome");
+		System.out.println("Welcome");
 	}
 	
 	@Test
